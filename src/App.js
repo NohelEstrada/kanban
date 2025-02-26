e5593d1638695a907e93c2dcfebc4724e006d420
NohelEstrada <dnestrada@unis.edu.gt>
2025-02-26 16:50:17 -0600

    First commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Kanban from './Kanban';
+
+function App() {
+  return (
+    <div className="App">
+      <Kanban />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Kanban.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Kanban = () => {
+  return (
+    <div>
+      <h1>Bienvenido a mi aplicación</h1>
+      <p>Este es el contenido principal de mi frontend.</p>
+    </div>
+  );
+};
+
+export default Kanban;
